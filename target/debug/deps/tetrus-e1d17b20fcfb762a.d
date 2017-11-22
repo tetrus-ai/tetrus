eeaa20adb5de375b5546a987cdec5f745ab0f3dd
Dan Cohen <dan.cohen@codurance.com>
2017-11-22 12:20:20 +0000

    update idea stuff

---
+++ target/debug/deps/tetrus-e1d17b20fcfb762a.d
@@ -0,0 +1,18 @@
+/Users/dancohen/Git/tetrus/target/debug/deps/tetrus-e1d17b20fcfb762a.rmeta: src/lib.rs src/game/mod.rs src/game/game.rs src/game/game_state.rs src/game/play_area_size.rs src/movements/mod.rs src/movements/piece_keeper.rs src/pieces/mod.rs src/pieces/position.rs src/pieces/tetromino_buffer.rs src/pieces/random_tetromino_stream.rs src/pieces/placed_piece.rs src/pieces/shape.rs src/rules.rs
+
+/Users/dancohen/Git/tetrus/target/debug/deps/tetrus-e1d17b20fcfb762a.d: src/lib.rs src/game/mod.rs src/game/game.rs src/game/game_state.rs src/game/play_area_size.rs src/movements/mod.rs src/movements/piece_keeper.rs src/pieces/mod.rs src/pieces/position.rs src/pieces/tetromino_buffer.rs src/pieces/random_tetromino_stream.rs src/pieces/placed_piece.rs src/pieces/shape.rs src/rules.rs
+
+src/lib.rs:
+src/game/mod.rs:
+src/game/game.rs:
+src/game/game_state.rs:
+src/game/play_area_size.rs:
+src/movements/mod.rs:
+src/movements/piece_keeper.rs:
+src/pieces/mod.rs:
+src/pieces/position.rs:
+src/pieces/tetromino_buffer.rs:
+src/pieces/random_tetromino_stream.rs:
+src/pieces/placed_piece.rs:
+src/pieces/shape.rs:
+src/rules.rs:
---
+++ target/debug/deps/tetrus-e83b2df943d8323a.d
@@ -0,0 +1,18 @@
+/Users/dancohen/Git/tetrus/target/debug/deps/tetrus-e83b2df943d8323a.rmeta: src/lib.rs src/game/mod.rs src/game/game.rs src/game/game_state.rs src/game/play_area_size.rs src/movements/mod.rs src/movements/piece_keeper.rs src/pieces/mod.rs src/pieces/position.rs src/pieces/tetromino_buffer.rs src/pieces/random_tetromino_stream.rs src/pieces/placed_piece.rs src/pieces/shape.rs src/rules.rs
+
+/Users/dancohen/Git/tetrus/target/debug/deps/tetrus-e83b2df943d8323a.d: src/lib.rs src/game/mod.rs src/game/game.rs src/game/game_state.rs src/game/play_area_size.rs src/movements/mod.rs src/movements/piece_keeper.rs src/pieces/mod.rs src/pieces/position.rs src/pieces/tetromino_buffer.rs src/pieces/random_tetromino_stream.rs src/pieces/placed_piece.rs src/pieces/shape.rs src/rules.rs
+
+src/lib.rs:
+src/game/mod.rs:
+src/game/game.rs:
+src/game/game_state.rs:
+src/game/play_area_size.rs:
+src/movements/mod.rs:
+src/movements/piece_keeper.rs:
+src/pieces/mod.rs:
+src/pieces/position.rs:
+src/pieces/tetromino_buffer.rs:
+src/pieces/random_tetromino_stream.rs:
+src/pieces/placed_piece.rs:
+src/pieces/shape.rs:
+src/rules.rs:
